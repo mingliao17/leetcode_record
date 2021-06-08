95bd41dcfa365012cd80f2271821c97a8833ca3a
mingliao17 <ideal9527@gmail.com>
2021-06-08 16:48:30 +0800

    solution_update

--- src/xyz/mingliao/s189旋转数组/Solution.java
+++ src/xyz/mingliao/s189旋转数组/Solution.java
@@ -2,30 +2,52 @@ package xyz.mingliao.s189旋转数组;
 
 public class Solution {
     /**
-     * 使用临时数组
-     * @param nums
-     * @param k
+     * 使用临时数组,最low的我的解法
      */
     public void rotate(int[] nums, int k) {
-        int[] new_nums = new int[nums.length];
-        int k_after=0;
-        for(int i=nums.length-k;i<nums.length;i++){
-            new_nums[k_after] = nums[i];
-            k_after++;
-        }
-        int k_before=nums.length-k-1;
-        for(int i=k;i>=0;i--){
-            new_nums[k_before] = nums[i];
-            k_before++;
-        }
-        nums = new_nums;
+        int length = nums.length;
+        int[] new_nums = new int[length];
 
-        for (int i : nums){
+        //这是最后加的，之前没考虑k比nums.length长的情况，也是看了官方题解的临时数组方法才考虑到的，这个和官方的差不了多少
+        k = k%length;
+
+        //先把k的这后半段放到新数组的前面
+        for (int i = 0; i < k; i++) {
+            new_nums[i] = nums[length - k + i];
+        }
+        //再把k之前的前半段放到新数组的后面
+        for (int i = k; i < length; i++) {
+            new_nums[i] = nums[i - k];
+        }
+        //把临时数组的内容给到nums
+        System.arraycopy(new_nums, 0, nums, 0, length);
+        //打印一下
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * 官方的临时数组解法
+     *
+     * 人家这个思路还是牛逼，但我不知道我差在了哪里
+     *
+     * 我知道我差再哪里了，没有考虑k的范围
+     */
+    public void rotate2(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+        //打印一下
+        for (int i : nums) {
             System.out.println(i);
         }
     }
 
     public static void main(String[] args) {
-       new Solution().rotate(new int[]{1,2,3,4,5,6,7},6);
+        new Solution().rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 8);
     }
 }
---
+++ src/xyz/mingliao/s412FizzBuzz/Solution.java
@@ -0,0 +1,98 @@
+package xyz.mingliao.s412FizzBuzz;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ */
+public class Solution {
+
+    /**
+     * 最简单的解法
+     */
+    public List<String> fizzBuzz(int n) {
+        List<String> stringList = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                stringList.add("FizzBuzz");
+            }else if (i%3==0){
+                stringList.add("Fizz");
+            }else if (i%5==0){
+                stringList.add("Buzz");
+            }else{
+                stringList.add(i+"");
+            }
+        }
+        return stringList;
+    }
+
+    /**
+     * 看了网络上相关的业务侧问题，重构上面的代码
+     *
+     * 这种耗时和占用内存要比上一种多那么一丢丢，但是对于业务的扩展来说是比较好的
+     */
+    public List<String> fizzBuzz2(int n){
+        List<String> stringList = new ArrayList<>();
+        String result = "";
+        for(int i=1;i<=n;i++){
+            if(i%3==0){
+                result+="Fizz";
+            }
+            if(i%5==0){
+                result+="Buzz";
+            }
+            if("".equals(result)){
+                result = i+"";
+            }
+            stringList.add(result);
+            result = "";
+        }
+        return stringList;
+    }
+
+    /**
+     * 题解中某个兄台的做法，果真是0ms的速度，超过了100%，直接重写了AbstractList，擦
+     *
+     * 这个就感觉有点飘忽忽了，目前对list的认知达不到这个程度，这玩意用get方法就能执行循环操作吗，这是咋操作的呢
+     */
+    public List<String> fizzBuzz3(int n){
+        return  new java.util.AbstractList<String>() {
+            @Override
+            public String get(int i) {
+                i++;
+                switch ((i % 3 == 0 ? 1 : 0) + (i % 5 == 0 ? 2 : 0)) {
+                    case 0:
+                        return String.valueOf(i);
+                    case 1:
+                        return "Fizz";
+                    case 2:
+                        return "Buzz";
+                    case 3:
+                        return "FizzBuzz";
+                    default:
+                        return "";
+                }
+            }
+            @Override
+            public int size() {
+                return n;
+            }
+        };
+    }
+
+
+    public static void main(String[] args) {
+        int n = 15;
+        Solution solution = new Solution();
+        List<String> fizzBuzzList = solution.fizzBuzz3(n);
+        for (String s : fizzBuzzList){
+            System.out.println(s);
+        }
+    }
+
+}
